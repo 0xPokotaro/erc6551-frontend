4cc880aa58b0b00cefc0d3633fd717e71dd0a7e3
pokotaro.eth <kentarou_masuda@hacklab-tech.io>
2023-06-20 17:32:17 +0900

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { WagmiConfig, createConfig, configureChains, mainnet, sepolia } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { publicClient, webSocketPublicClient } = configureChains(
+    [mainnet, sepolia],
+    [publicProvider()],
+  )
+
+  const config = createConfig({
+    autoConnect: true,
+    publicClient,
+    webSocketPublicClient,
+  })
+
+  return (
+    <WagmiConfig config={config}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  )
 }
